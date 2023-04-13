27409c093dc3a884bea2d50ed851b8c23be0e783
Jacobo91 <jacobovelasquez1991@gmail.com>
2023-04-13 15:03:06 -0500

    'fix video support'

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
 import video from '../images/sunrise-timelapse.mp4';
+import videoWebm from '../images/sunrise-timelapse.webm';
+import videoOgg from '../images/sunrise-timelapse.ogg';
 import logoLetters from '../images/logo-letters.png';
 
 export function Hero(){
     return(
         <section className="hero">
-            <video  muted loop>
+            <video  autoPlay muted loop>
                 <source src={video} type="video/mp4" />
+                <source src={videoWebm} type="video/webm" />
+                <source src={videoOgg} type="video/ogg" />
             </video>
             <img src={logoLetters} alt="" className='logo-letters'/>
         </section>
